09a308a9e17357b59ba223f666e74920ab8b7291
alhrmthon <107150406+alhrmthon@users.noreply.github.com>
2022-07-10 12:37:34 +0200

    Add files via upload

--- unv.py
+++ unv.py
@@ -1,5 +1,6 @@
-import telebot,random,os,pyrogram
+import telebot,random,os,pyrogram,asyncio
 from telebot import types
+from pyrogram import Client,filters,enums
 import py_compile,json,base64, marshal, zlib,os
 botf = True
 
